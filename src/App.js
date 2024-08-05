f633f0ae9f9206a4dc0cce98f7c6d70fc162de83
gonchigars <gonchigars@gmail.com>
2024-08-05 07:33:10 +0530

    Day2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from "react";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import React, { useState } from "react";
+import { CssBaseline, ThemeProvider, createTheme, Box } from "@mui/material";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#2196f3", // Blue color similar to the image
+      main: "#2196f3",
     },
   },
 });
 
 function App() {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div className="App">
-        <Header />
-        {/* Other components will go here */}
-      </div>
+      <Box sx={{ display: "flex" }}>
+        <Header handleDrawerToggle={handleDrawerToggle} />
+        <Sidebar
+          mobileOpen={mobileOpen}
+          handleDrawerToggle={handleDrawerToggle}
+        />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          {/* Main content will go here */}
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 }
