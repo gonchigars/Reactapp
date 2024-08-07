21dca207377807aa04aa9c0c1567976c109b11ba
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-08-07 15:18:02 +0530

    change

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,49 +1,57 @@
-import React from "react";
-import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Toolbar,
-} from "@mui/material";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import React from 'react';
+import { Drawer, List, ListItem, Button} from '@mui/material';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { blue } from '@mui/material/colors';
 
-const drawerWidth = 240;
-
-function ResponsiveDrawer({ mobileOpen, handleDrawerToggle }) {
-  const drawer = (
-    <div>
-      <Toolbar /> {/* This empty Toolbar acts as a spacer */}
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
+const SidebarWithButton = () => {
   return (
-    <Drawer
-      variant="permanent"
-      sx={{
-        display: { xs: "none", sm: "block" },
-        "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
-      }}
-      open
-    >
-      {drawer}
+    <Drawer variant="permanent" sx={{  '& .MuiDrawer-paper': {
+        width: 220,
+        backgroundColor: 'black',color:'white',marginTop:8}}}>
+      <List>
+        <ListItem>
+          <Button variant="contained" sx={{background:'red',color:'white'}}>Categories</Button>
+        </ListItem>
+        <ListItem>
+        <i className="fas fa-clapperboard" style={{ fontSize: '15px', color:'red' }}></i>
+
+
+          <Button sx={{color:'white'}} >Popular</Button>
+        </ListItem>
+        <ListItem>
+        <i class="fa-solid fa-star"style={{fontSize:'15px',color:'red'}}></i>
+
+        <Button  sx={{color:'white'}} >Top Rated</Button>
+
+        </ListItem>
+        <ListItem>
+        <i className="fas fa-sun" style={{ fontSize: '16px', color: 'red' }}></i>
+        <Button  sx={{color:'white'}}>Upcoming</Button>
+        </ListItem>
+        <ListItem>
+          <Button variant="contained" sx={{background:'red'}}>Genres</Button>
+        </ListItem>
+        <ListItem>
+        <i className="fas fa-film" style={{ fontSize: '15px', color: 'red' }}></i>   
+                 <Button  sx={{color:'white'}}>Action</Button>
+        </ListItem>
+        <ListItem>
+        <i className="fas fa-film" style={{ fontSize: '15px', color: 'red' }}></i>   
+            <Button  sx={{color:'white'}}>Adventure</Button>
+        </ListItem>
+        <ListItem>
+        <i className="fas fa-bug" style={{ fontSize: '15px', color: 'red' }}></i>
+        <Button  sx={{color:'white'}}>Animation</Button>
+        </ListItem>
+        <ListItem>
+        <i className="fas fa-laugh" style={{ fontSize: '15px', color: 'red' }}></i>
+
+            <Button  sx={{color:'white'}}>Comedy</Button>
+        </ListItem>
+        </List>
     </Drawer>
   );
-}
+};
+export default SidebarWithButton;
 
-export default ResponsiveDrawer;
+
---
+++ src/config/tmdb.js
@@ -0,0 +1,3 @@
+export const TMDB_API_KEY = "ce07cbbca3b3f3e5f2b12dbc4aa85157";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
