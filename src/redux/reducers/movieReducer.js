7e477f5987565f91b1df468c457402793188affa
syamkarthik0 <dharmavarapukarthik@gmail.com>
2024-08-12 22:23:40 +0530

    From Karthik

--- src/redux/reducers/movieReducer.js
+++ src/redux/reducers/movieReducer.js
@@ -1,18 +1,52 @@
-// reducers/movieReducer.js
 const initialState = {
-  movies: [],
-};
+    movies: [],
+  };
+  
+  const movieReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "SET_MOVIES":
+        return {
+          ...state,
+          movies: action.payload,
+        };
+  
+      case "ADD_MOVIE":
+        return {
+          ...state,
+          movies: [...state.movies, action.payload],
+        };
+  
+      case "REMOVE_MOVIE":
+        return {
+          ...state,
+          movies: state.movies.filter((movie) => movie.id !== action.payload),
+        };
+  
+      case "UPDATE_MOVIE":
+        return {
+          ...state,
+          movies: state.movies.map((movie) =>
+            movie.id === action.payload.id
+              ? { ...movie, ...action.payload }
+              : movie
+          ),
+        };
+  
+      case "CLEAR_MOVIES":
+        return {
+          ...state,
+          movies: [],
+        };
 
-const movieReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "SET_MOVIES":
-      return {
-        ...state,
-        movies: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-
-export default movieReducer;
+      case "FETCH_MOVIES":
+        return{
+          ...state,
+          movies: action.payload,
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default movieReducer;
